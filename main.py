2df6fcc29ffb3c93dfe120305df1b9891a3ceadd
Chris Dail <chris.dail@dashsocial.com>
2025-07-23 17:54:59 -0300

    Improved RO protection and added better docs to the AI

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
-from typing import Any, Dict, List
-import os
-
 from mcp_server import mcp
 
 import mysql_tools
 import onepssword
-
-
-RO_DB_ENTRIES = os.getenv('RO_DB_ENTRIES', '').split(',') or []
-RW_DB_ENTRIES = os.getenv('RW_DB_ENTRIES', '').split(',') or []
-
-
-@mcp.tool()
-def available_databases() -> Dict[str, List[str]]:
-    return {
-        'read_only_databases': RO_DB_ENTRIES,
-        'read_write_databases': RW_DB_ENTRIES,
-    }
 
 
 if __name__ == '__main__':
--- mysql_tools.py
+++ mysql_tools.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict
+from typing import Any, Dict, List
 import os
 import pymysql
 from fastmcp.utilities.logging import get_logger
@@ -6,8 +6,9 @@ from fastmcp.exceptions import ToolError
 from mcp_server import mcp
 import onepssword
 
-RO_DB_ENTRIES = os.getenv('RO_DB_ENTRIES', '').split(',')
-RW_DB_ENTRIES = os.getenv('RW_DB_ENTRIES', '').split(',')
+
+RO_DB_ENTRIES = os.getenv('RO_DB_ENTRIES', '').split(',') or []
+RW_DB_ENTRIES = os.getenv('RW_DB_ENTRIES', '').split(',') or []
 
 logger = get_logger(__name__)
 
@@ -15,8 +16,8 @@ logger = get_logger(__name__)
 def is_safe_query(sql: str) -> bool:
     """Check for potentially unsafe queries"""
     sql_lower = sql.lower()
-    unsafe_keywords = ['insert', 'update', 'delete', 'drop', 'alter', 'truncate', 'create']
-    return not any(keyword in sql_lower for keyword in unsafe_keywords)
+    unsafe_keywords = ['INSERT ', 'UPDATE ', 'DELETE ', 'DROP ', 'ALTER ', 'TRUNCATE ', 'CREATE ', 'SET ']
+    return not any(keyword in sql.upper() for keyword in unsafe_keywords)
 
 
 def is_read_only_database(database: str) -> bool:
@@ -25,10 +26,27 @@ def is_read_only_database(database: str) -> bool:
 
 
 @mcp.tool()
-def run_query(database: str, sql: str) -> Dict[str, Any]:
+def available_databases() -> Dict[str, List[str]]:
+    """
+    Returns the available databases and if they are read-only or read-write.
+    These are suitable for use in the `database` parameter of the `run_query` tool.
+    """
+    return {
+        'read_only_databases': RO_DB_ENTRIES,
+        'read_write_databases': RW_DB_ENTRIES,
+    }
+
+
+@mcp.tool()
+def sql_query(database: str, sql: str) -> Dict[str, Any]:
+    """
+    Executes a SQL query on the specified database.
+    The `database` parameter should be one of the databases returned by the `available_databases` tool.
+    The `sql` parameter should be a valid SQL query. Add LIMIT 100 to SELECT queries intended to return multipls rows.
+    """
+
     if not is_safe_query(sql) and is_read_only_database(database):
         raise ToolError('Only SELECT queries are allowed for read-only databases')
-
 
     logger.info(f'Looking up details for database: {database}')
     db_args = onepssword.get_mysql_db_args(database)
